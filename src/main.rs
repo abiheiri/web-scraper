5c81d44381d5a13f00102e67ea448df47795956c
Al <al@forgottheaddress.com>
2026-01-30 19:40:50 -0800

    fix the versioning

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::time::Duration;
 use url::Url;
 
 const PROG: &str = "web-scraper";
-const VERSION: &str = "2.0.0";
+const VERSION: &str = "1g.0.0";
 const AUTHOR: &str = "Al Biheiri (al@forgottheaddress.com)";
 const HTTP_TIMEOUT: u64 = 10;
 
